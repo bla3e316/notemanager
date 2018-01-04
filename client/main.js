da8902f50ea0d9e8a1979959f8d1b7c3dc3401ac
Aleks <bla3e316@gmail.com>
2018-01-04 13:54:27 +0200

    add event template for add notes button

--- client/main.js
+++ client/main.js
@@ -14,3 +14,27 @@ Template.body.helpers({
     return Notes.find({});
   }
 });
+
+Template.add.events({
+  'submit .add-form': function(){
+    event.preventDefault();
+
+    //get input value
+    const target = event.target;
+    const text = target.text.value;
+
+    //insert note into collection
+    Notes.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    //clean form
+    target.text.value = '';
+
+    //close Modal
+    $('#addModal').modal('close');
+
+    return false;
+  }
+});
